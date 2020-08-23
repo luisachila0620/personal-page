70821ee395c46df481a2c900861fca1d29b612f5
luisachila0620 <luisarchila038@gmail.com>
2020-08-23 17:04:05 +0000

    chaging plans

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,21 +3,21 @@ import React from "react";
 //create your first component
 export function Home() {
 	return (
-		<div>
-			<div classNameName="fondo">
-				<div className="title">
-					<h1>welcome</h1>
-				</div>
-				<img
-					className="fondo1"
-					src="https://c0.wallpaperflare.com/preview/794/155/609/crack-grunge-wall-grey-dark.jpg"
-					classNameName="card-img"
-					alt="..."
-				/>
-
-				<div classNameName="single-img">
-					<img src="https://image.winudf.com/v2/image1/Y29tLkNvb2xXYWxscGFwZXJzX3NjcmVlbl8wXzE1NDg1NTg0NzVfMDEz/screen-0.jpg?fakeurl=1&type=.jpg" />
-				</div>
+		<div className="imagen-fondo">
+			<img
+				src="https://i.pinimg.com/originals/74/61/a0/7461a0d7be9806dc855c9ab793e65aba.jpg"
+				className="card-img"
+				alt="military image"
+			/>
+			<div className="parrafos">
+				<h5 className="card-title">
+					WE HONOR AND RESPECT THE SACRIFICE OUR TROOPS MAKE.
+				</h5>
+				<p className="card-text">
+					THIS IS A WEBSITE TO HONOR OUR TROOPS AND FALLEN HEROS IN
+					THE NAME OF FREEDOM.
+				</p>
+				<p className="card-text">Last updated 3 mins ago</p>
 			</div>
 		</div>
 	);
